6731de3fb3bee4a9e5801399566ae8472e28ddd7
jammie-commits <jay.mbugua.ph@gmail.com>
2024-05-25 17:23:27 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    if length > 0:
+        fibonacci_sequence.append(0)
+    if length > 1:
+        fibonacci_sequence.append(1)    
+    
+    for i in range(2, length):
+        next_number = fibonacci_sequence[i-1] + fibonacci_sequence[i-2]
+        fibonacci_sequence.append(next_number)
+
+    print(fibonacci_sequence)
+
+print_fibonacci(10)       
